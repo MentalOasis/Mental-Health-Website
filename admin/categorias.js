451f9d7097fd67ae98605efa998697770bbe2177
ShengtzyS <shengtzy.subaique@genstudents.org>
2024-05-22 19:01:17 -0400

    integrando funciones js

---
+++ admin/categorias.js
@@ -0,0 +1,36 @@
+function alerta(){
+    document.getElementById("AInicio") 
+        window.alert("Está siendo dirigido a la pestaña Inicio");
+}
+
+function alerta1(){
+    document.getElementById("AConfiguración") 
+        window.alert("Está siendo dirigido a la pestaña Configuración");
+}
+
+function alerta2(){
+    document.getElementById("AEditarpregunta") 
+        window.alert("Está siendo dirigido a la pestaña Editar Preguntas");
+}
+
+function alerta3(){
+    document.getElementById("ACrearpregunta") 
+        window.alert("Está siendo dirigido a la pestaña Crear Preguntas");
+}
+
+function alerta4(){
+    document.getElementById("ACategorias") 
+        window.alert("Está siendo dirigido a la pestaña Categorías");
+}
+
+
+
+
+function hacerClick() {
+    var temaNuevo = prompt("Ingresa el tema que quiere modificar: ");
+    console.log("El tema modificado: " + temaNuevo)
+};
+
+
+
+
